be4f098e3bf085572d127543cad74d880d12d73c
Andrew Brandenfels <brandena@oregonstate.edu>
2025-02-01 15:59:17 -0800

    Bug fixes in edit_distance and backtracking

--- gene_seq_checker.py
+++ gene_seq_checker.py
@@ -10,8 +10,10 @@ def edit_distance(A, B, loss_matrix, x_indexdict, y_indexdict):
     Edit[0, 0] = loss_matrix[1][1]
     for j in range(1, n + 1):
         Edit[0][j] = loss_matrix[1][x_indexdict[B[j - 1]]]
+        ptr[0][j] = 0
     for i in range (1, m + 1):
         Edit[i][0] = loss_matrix[y_indexdict[A[i - 1]]][1]
+        ptr[i][0] = 1
 
     for i in range (1, m + 1):
         for j in range (1, n + 1):
@@ -51,16 +53,16 @@ def read_sequences(inputfile):
     with open(inputfile, 'r') as file:
         return [line.strip().split(',') for line in file]
 
-def write_output(alignments):
+def write_output(alignments, outputfile):
     # Writes alignment result to imp3output.txt (we can change the naming later)
-    with open("imp3output.txt", 'w') as file:
+    with open(outputfile, 'w') as file:
         for seq1, seq2, cost in alignments:
             file.write(f"{seq1},{seq2}:{cost}\n")
 
 def compute_alignment(A, B, loss_matrix, x_indexdict, y_indexdict):
     cost, ptr = edit_distance(A, B, loss_matrix, x_indexdict, y_indexdict)
     alignment = backtrack_alignment(A, B, ptr, len(A), len(B))
-    return alignment, cost
+    return alignment[0], alignment[1], cost
 
 def main(argv):
     print('_' * 100)
@@ -76,7 +78,7 @@ def main(argv):
     
     sequences = read_sequences(inputfile)
     alignments = [compute_alignment(A, B, loss_matrix, x_indexdict, y_indexdict) for A, B in sequences]
-    write_output(alignments)    
+    write_output(alignments, outputfile)    
 
 if __name__ == "__main__":
     main(sys.argv[1:])
